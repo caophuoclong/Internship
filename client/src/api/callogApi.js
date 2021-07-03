5c8e65a9e4f98f84e30d82ef417fd4bc8946cf0c
cakcak2001 <caophuoclong1@gmail.com>
2021-07-03 14:32:04 +0700

    Update gitignore

--- client/src/api/callogApi.js
+++ client/src/api/callogApi.js
@@ -1,10 +1,6 @@
 const { default: axiosClient } = require("./axiosClient");
 
 module.exports = {
-  createacallog: (params) => {
-    const url = "/createacallog";
-    return axiosClient.post(url, { params });
-  },
   testApi: () => {
     const url = "/testapi";
     return axiosClient.get(url);
