df5596f93f2c94e989b9a6759d045ba8c3773515
cakcak2001 <56899834+Caophuoclong@users.noreply.github.com>
2021-07-03 14:23:43 +0700

    Update axiosClient.js

--- client/src/api/axiosClient.js
+++ client/src/api/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import queryString from "query-string";
 const axiosClient = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: "http://localhost:5000/api",
   withCredentials: false,
   headers: {
     "Content-Type": "application/json",
